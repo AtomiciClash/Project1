1e55088532fa7ebd632ac7ff0a0c3de7eef83116
AtomiciClash <bd569401@ahschool.com>
2025-11-19 15:18:42 -0500

    Update sketch_nov12a_copy_20251117153110.ino

--- sketch_nov12a_copy_20251114150013/sketch_nov12a_copy_20251117153110/sketch_nov12a_copy_20251117153110.ino
+++ sketch_nov12a_copy_20251114150013/sketch_nov12a_copy_20251117153110/sketch_nov12a_copy_20251117153110.ino
@@ -4,63 +4,74 @@
 
 MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
-
+MeRGBLed led(0,30); 
 
 void forward() {
   motor1.run(-100);
   motor2.run(100);
+  led.setColor(0,255,0);
+  led.show();
 }
 
 void backward() {
   motor1.run(100);
   motor2.run(-100);
+  led.setColor(0,255,0);
+  led.show();
 }
 
 void turnRight(){
   motor1.run(-100);
   motor2.run(-100);
+  led.setColor(0,255,0);
+  led.show();
 }
 
 void turnLeft(){
   motor1.run(100);
   motor2.run(100);
+  led.setColor(0,255,0);
+  led.show();
 }
 
 void stop(){
   motor1.stop();
   motor2.stop();
+  led.setColor(255,0,0);
+  led.show();
 }
 void setup() {
+ led.setpin(13);
 pinMode(A7,INPUT);
  while(analogRead(A7) != 0);
  forward();
- delay(4600); // First straight
+ delay(4900); // First straight
  stop();
- delay(500);
+ delay(3000);
 
 
  turnLeft(); // First right turn (Going backwards so it's left)
- delay(515);
+ delay(545);
  stop();
-  delay(500);
+  delay(3000);
 
 
  backward(); // Backwards driving!
  delay(3900);
  stop();
-  delay(500);
+  delay(3000);
 
 
  turnRight(); // Next Turn
- delay(503);
+ delay(540);
  stop();
-  delay(500);
+  delay(3000);
 
 
  forward(); // Last straight
- delay(4500);
+ delay(5100);
  stop();
-  delay(500);
+  delay(3000);
 
 
   turnLeft(); // Turn for a flex
